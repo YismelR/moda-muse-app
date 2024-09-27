cb142d2668556ad9a86f7af583aef0d6cea96db8
YismelR <yismel_rodriguez@outlook.com>
2024-09-26 22:03:07 -0400

    fixed the logo sizing

--- src/components/homepage/HighLight.tsx
+++ src/components/homepage/HighLight.tsx
@@ -2,9 +2,9 @@ import logo from "@/assets/logo-highlight.svg"
 export default function HighLight(){
     return(
     <section className="bg-greeHighlight h-[473px] flex flex-col place-items-center gap-1 pt-14 pb-28 shadow-2xl">
-        <img src={logo} alt="logo highlight" className="h-28"/>
+        <img src={logo} alt="logo highlight" className="h-20 sm:h-28"/>
         <div className="flex flex-col place-items-center max-w-lg gap-2">
-        <h1 className="text-7xl text-logoText font-manrope font-extralight tracking-tight">Moda Muse</h1>
+        <h1 className="text-5xl md:text-7xl text-logoText font-manrope font-extralight tracking-tight">Moda Muse</h1>
         <p className="text-center text-greyText">At Moda Muse, style meets convenience.<br/> 
         Explore curated collections, find your inspiration,<br/>
     and elevate your wardrobe with a seamless shopping experience!</p>
--- src/components/navigation-bar/Logo.tsx
+++ src/components/navigation-bar/Logo.tsx
@@ -1,5 +1,5 @@
 import logo from "@/assets/logo.svg";
 
 export default function Logo() {
-  return <img src={logo} alt="logo" className="h-20" />;
+  return <img src={logo} alt="logo" className="h-10 md:h-16 lg:h-20" />;
 }
