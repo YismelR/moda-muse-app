e8bc8a8ca20c9c5d888a3839cb7f8fbe9d5661d5
YismelR <yismel_rodriguez@outlook.com>
2024-09-27 11:12:17 -0400

    changed the features to be in a flex

--- src/components/homepage/Features.tsx
+++ src/components/homepage/Features.tsx
@@ -1,11 +1,13 @@
-import FeatureCards from "./FeatureCards";
+import FeatureCards from './FeatureCards';
 
 export default function Features() {
   return (
-    <section className="flex flex-col gap-12 my-24 px-16">
-      <h1 className="text-2xl">Features</h1>
-      <div className="max-w-full">
-        <FeatureCards />
+    <section className="flex justify-center">
+      <div className="flex flex-col gap-12 my-24 px-9 xl:px-0">
+        <h1 className="text-2xl">Features</h1>
+        <div className="max-w-full">
+          <FeatureCards />
+        </div>
       </div>
     </section>
   );
