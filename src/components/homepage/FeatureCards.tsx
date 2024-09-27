2b4e136006a5e0a5cac4a8cef9f91ff22392ee2b
YismelR <yismel_rodriguez@outlook.com>
2024-09-26 21:27:49 -0400

    applied the feature cards

--- src/components/homepage/FeatureCards.tsx
+++ src/components/homepage/FeatureCards.tsx
@@ -1,39 +1,58 @@
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import women from "@/assets/women.svg"
-import men from "@/assets/men.svg"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import women from "@/assets/women.svg";
+import men from "@/assets/men.svg";
+import kids from "@/assets/kids.svg";
+import shoes from "@/assets/shoes.svg";
+import accessories from "@/assets/accessories.svg";
+import handbags from "@/assets/handbags.svg";
 
 
-const cardCategories= [{
-  title: "Women",
-  src: women,
-  alt: "women"
-},
-{
-  title:"Men",
-  src:men,
-  alt: "men"
-},
-]
+const cardCategories = [
+  {
+    title: "Women",
+    src: women,
+    alt: "women",
+  },
+  {
+    title: "Men",
+    src: men,
+    alt: "men",
+  },
+  {
+    title: "Kids",
+    src: kids,
+    alt: "kids",
+  },
+  {
+    title: "Shoes",
+    src: shoes,
+    alt: "shoes",
+  },
+  {
+    title: "Accessories",
+    src: accessories,
+    alt: "accessories",
+  },
+  {
+    title: "Handbags",
+    src: handbags,
+    alt: "handbags",
+  },
+];
 
-export default function FeatureCards(){
-    return (
-      <div>
-{cardCategories.map((category, idx) => (
-
-  <Card key={idx}>
-  <CardHeader>
-    <CardTitle>{category.title}</CardTitle>
-  </CardHeader>
-  <CardContent>
-    <img src={category.src} alt={category.alt}/>
-  </CardContent>
-</Card>
-    ))}
-      </div>
-)
+export default function FeatureCards() {
+  return (
+    <div className="grid grid-cols-3 gap-12">
+      {cardCategories.map((category, idx) => (
+        <Card key={idx}>
+          <CardHeader>
+            <CardTitle className="text-xl">{category.title}</CardTitle>
+          </CardHeader>
+          <CardContent className="p-0">
+            <img src={category.src} alt={category.alt} className="w-full rounded-b-xl" />
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
 }
--- src/components/homepage/Features.tsx
+++ src/components/homepage/Features.tsx
@@ -1,12 +1,12 @@
 import FeatureCards from "./FeatureCards";
 
-export default function Features(){
-    return(
-        <section>
-            <h1 className="text-2xl">Features</h1>
-            <div>
-            <FeatureCards/>
-            </div>
-        </section>
-    )
+export default function Features() {
+  return (
+    <section className="flex flex-col gap-12 my-24 px-16">
+      <h1 className="text-2xl">Features</h1>
+      <div className="max-w-full">
+        <FeatureCards />
+      </div>
+    </section>
+  );
 }
