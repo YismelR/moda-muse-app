d5e8d2afa968013fd657f689bf52f8c8c9949e20
YismelR <yismel_rodriguez@outlook.com>
2024-09-28 01:52:12 -0400

    linked the logo on the nav to go to the home page

--- src/components/navigation-bar/Logo.tsx
+++ src/components/navigation-bar/Logo.tsx
@@ -1,5 +1,10 @@
-import logo from "@/assets/logo.svg";
+import logo from '@/assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
-  return <img src={logo} alt="logo" className="h-10 md:h-16 lg:h-20" />;
+  return (
+    <Link to="/">
+      <img src={logo} alt="logo" className="h-10 md:h-16 lg:h-20" />
+    </Link>
+  );
 }
