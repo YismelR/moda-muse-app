f50e7c3b1acc195f32a6457282f1a1021aa82eb9
YismelR <yismel_rodriguez@outlook.com>
2024-10-19 21:35:18 -0400

    applied a margin top to the items list applied a shadow to the header

--- src/components/item-list/ItemsList.tsx
+++ src/components/item-list/ItemsList.tsx
@@ -4,7 +4,7 @@ import Search from './Search';
 
 export default function ItemsList() {
   return (
-    <main className="grid grid-cols-12 mx-9 place-self-center xl:mx-0 max-w-screen-xl">
+    <main className="grid grid-cols-12 mx-9 place-self-center xl:mx-0 max-w-screen-xl mt-8">
       <Filter />
       <div className="col-span-9">
         <Search />
--- src/components/navigation-bar/NavBar.tsx
+++ src/components/navigation-bar/NavBar.tsx
@@ -1,8 +1,8 @@
-import Logo from "./Logo";
-import NavButtons from "./NavButtons";
+import Logo from './Logo';
+import NavButtons from './NavButtons';
 export default function NavBar() {
   return (
-    <header className="px-8 py-4 flex justify-between place-items-center">
+    <header className="px-8 py-4 flex justify-between place-items-center shadow-md">
       <Logo />
       <NavButtons />
     </header>
