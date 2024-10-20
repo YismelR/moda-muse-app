60f0c5065b534b7006e53d72248e165c589e6c0d
YismelR <yismel_rodriguez@outlook.com>
2024-10-19 22:21:50 -0400

    added a search bar, removed margin top from the items,

--- src/components/item-list/ItemsList.tsx
+++ src/components/item-list/ItemsList.tsx
@@ -6,9 +6,9 @@ export default function ItemsList() {
   return (
     <main className="grid grid-cols-12 mx-9 place-self-center xl:mx-0 max-w-screen-xl mt-8">
       <Filter />
-      <div className="col-span-9">
+      <div className="grid gap-12 col-span-9">
         <Search />
-        <div className="grid lg:grid-cols-3 gap-6 md:grid-cols-2 my-8">
+        <div className="grid lg:grid-cols-3 gap-6 md:grid-cols-2 ">
           <Items />
         </div>
       </div>
--- src/components/item-list/Search.tsx
+++ src/components/item-list/Search.tsx
@@ -1,7 +1,22 @@
+import { Form } from 'react-router-dom';
+import searchIcon from '@/assets/searchIcon.svg';
+
 export default function Search() {
   return (
     <section>
-      <h1>Search</h1>
+      <Form>
+        <input
+          type="search"
+          className="h-10 border w-full max-w-96 rounded-full pr-9 pl-4 shadow-sm outline-none"
+          style={{
+            backgroundImage: `url(${searchIcon})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: '20px 20px',
+            backgroundPosition: 'right 10px center',
+          }}
+          placeholder="Search"
+        />
+      </Form>
     </section>
   );
 }
